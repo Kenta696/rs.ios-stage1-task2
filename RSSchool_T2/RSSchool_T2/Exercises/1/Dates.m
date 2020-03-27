556a5e744e66f9c56e5b4c8201efc65c5c991ab3
Roman Krutev <61172690+Kenta696@users.noreply.github.com>
2020-03-27 12:39:31 +0300

    fix bug

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -18,13 +18,22 @@
     NSArray* arrayWithMonth = [NSArray arrayWithObjects:@"января,",@"феараля,",@"марта,",@"апреля,",@"мая,",
                                @"июня,",@"июля,",@"августа,",@"сентября,",
                                @"октября,",@"ноября,",@"декабря,",nil];
-    NSArray* arrayWithDay = [NSArray arrayWithObjects:@"понедельник",@"вторник",
-                             @"среда",@"четверг",@"пятница",@"суббота",@"воскресенье", nil];
+    NSArray* arrayWithDay = [NSArray arrayWithObjects:@"воскресенье",@"понедельник",@"вторник",
+                             @"среда",@"четверг",@"пятница",@"суббота", nil];
     NSInteger dayOfWeek = [dataSet weekday];
     NSCalendar* calendar = [NSCalendar currentCalendar];
     NSDate* data = [calendar dateFromComponents:dataSet];
     [calendar getEra:NULL yearForWeekOfYear:NULL weekOfYear:NULL weekday:&dayOfWeek fromDate:data];
-    if (numberMonth<=12 && numberDay <=31) {
+    if (numberMonth==2 && numberDay<=28) {
+        NSString* stringWithDay = [arrayWithDay objectAtIndex:dayOfWeek-1];
+        NSString* stringWithMonth = [arrayWithMonth objectAtIndex:numberMonth-1];
+        [string appendString:day];
+        [string appendString:@" "];
+        [string appendString:stringWithMonth];
+        [string appendString:@" "];
+        [string appendString:stringWithDay];
+    }
+    if (numberMonth<=12 && numberDay <=31 && numberMonth!=2 ) {
         NSString* stringWithDay = [arrayWithDay objectAtIndex:dayOfWeek-1];
         NSString* stringWithMonth = [arrayWithMonth objectAtIndex:numberMonth-1];
         [string appendString:day];
